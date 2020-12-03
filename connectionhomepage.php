efc2fde22fdb0742a45a37433cc058b44ac44472
Brandon <brandon.berry994@gmail.com>
2020-12-03 11:20:42 -0500

    added displays tables

--- connectionhomepage.php
+++ connectionhomepage.php
@@ -1,41 +1,190 @@
+<!DOCTYPE html>
 <html>
+<head>
+<title>Table with database</title>
+<style>
+table {
+border-collapse: collapse;
+width: 100%;
+color: #588c7e;
+font-family: monospace;
+font-size: 25px;
+text-align: left;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
+</head>
 <body>
-<?php 
-$host = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "student athlete health tracker"; 
-$conn = mysqli_connect($host, $dbusername, $dbpassword, $dbname); 
-$query = "SELECT * FROM studentathlete";
-
-
-echo '<table border="0" cellspacing="2" cellpadding="2"> 
-      <tr> 
-          <td> <font face="Arial">Value1</font> </td> 
-          <td> <font face="Arial">Value2</font> </td> 
-          <td> <font face="Arial">Value3</font> </td> 
-          <td> <font face="Arial">Value4</font> </td> 
-          <td> <font face="Arial">Value5</font> </td> 
-      </tr>';
-
-if ($result = $mysqli->query($query)) {
-    while ($row = $result->fetch_assoc()) {
-        $studentid = $row["StudentID"];
-        $firstname = $row["FirstName"];
-        $lastname = $row["LastName"];
-        $sportname = $row["SportName"];
-        $position = $row["Position"]; 
-
-        echo '<tr> 
-                  <td>'.$studentid.'</td> 
-                  <td>'.$firstname.'</td> 
-                  <td>'.$lastname.'</td> 
-                  <td>'.$sportname.'</td> 
-                  <td>'.$position.'</td> 
-              </tr>';
-    }
-    $result->free();
-} 
+<table>
+<tr>
+<th>Student ID</th>
+<th>First Name</th>
+<th>Last Name</th>
+<th>Sport</th>
+<th>Position</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "student athlete health tracker");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+//Data from StudentAthlete
+$sql = "SELECT StudentID, FirstName, LastName, SportName, Position FROM studentathlete";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["StudentID"]. "</td><td>" . $row["FirstName"] . "</td><td>"
+. $row["LastName"]. "</td></tr>" . $row["SportName"] . "</td></tr>" . $row["Position"] . "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
 ?>
+</table>
+</body>
+
+<body>
+<table>
+<tr>
+<th>Weight</th>
+<th>Blood Pressure</th>
+<th>Avg Rest Heart Rate</th>
+<th>Avg Active Heart Rate</th>
+<th>VO2 Max</th>
+<th>ECG</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "student athlete health tracker");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+//Data from StudentAthlete
+$sql = "SELECT BaseWeight, BaseBloodPressure, BaseAvgRestingHR, BaseAvgActiveHR, BaseVO2Max, BaseECG FROM baselinedata";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["BaseWeight"]. "</td><td>" . $row["BaseBloodPressure"] . "</td><td>"
+. $row["BaseAvgRestingHR"]. "</td></tr>" . $row["BaseAvgActiveHR"] . "</td></tr>" . $row["BaseVO2Max"] . "</td></tr>"
+. $row["BaseECG"] . "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+</body>
+
+<body>
+<table>
+<tr>
+<th>Weight</th>
+<th>Number Meals Eaten</th>
+<th>Hours of Sleep</th>
+<th>Blood Pressure</th>
+<th>Avg Rest Heart Rate</th>
+<th>Avg Active Heart Rate</th>
+<th>Abnormal Symptoms</th>
+<th>Health Tests Taken</th>
+<th>Test Results</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "student athlete health tracker");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+//Data from StudentAthlete
+$sql = "SELECT DailyWeight, MealsEaten, HoursSlept, DailyBloodPressure, DailyAvgRestingHR, DailyAvgActiveHR, DailyAbnormalSymptoms, DailyHealthTestTaken, DailyTestResults FROM dailydata";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["DailyWeight"]. "</td><td>" . $row["MealsEaten"] . "</td><td>"
+. $row["HoursSlept"]. "</td></tr>" . $row["DailyBloodPressure"] . "</td></tr>" . $row["DailyAvgRestingHR"] . "</td></tr>"
+. $row["DailyAvgActiveHR"] . "</td></tr>" . $row["DailyAbnormalSymptoms"] . "</td></tr>"
+. $row["DailyHealthTestTaken"] . "</td></tr>" . $row["DailyTestResults"] . "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+</body>
+
+<body>
+<table>
+<tr>
+<th>Weight</th>
+<th>Blood Pressure</th>
+<th>Avg Rest Heart Rate</th>
+<th>Avg Active Heart Rate</th>
+<th>VO2 Max</th>
+<th>ECG</th>
+<th>Abnormal Symptoms</th>
+<th>Health Tests Taken</th>
+<th>Test Results</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "student athlete health tracker");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+//Data from StudentAthlete
+$sql = "SELECT SickWeight, SickBloodPressure, SickAvgRestingHR, SickAvgActiveHR, SickVO2Max, SickECG, 
+SickAbnormalSymptoms, SickHealthTestTaken, SickTestResults FROM sickdata";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["SickWeight"]. "</td><td>" . $row["SickBloodPressure"] . "</td><td>"
+. $row["SickAvgRestingHR"]. "</td></tr>" . $row["SickAvgActiveHR"] . "</td></tr>" . $row["SickVO2Max"] . "</td></tr>"
+. $row["SickECG"] . "</td></tr>" . $row["SickAbnormalSymptoms"] . "</td></tr>"
+. $row["SickHealthTestTaken"] . "</td></tr>" . $row["SickTestResults"] . "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+</body>
+
+<body>
+<table>
+<tr>
+<th>Participation</th>
+<th>Workout</th>
+<th>Avg Heart Rate</th>
+<th>Injuries or Pains</th>
+<th>Test Results</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "student athlete health tracker");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+//Data from StudentAthlete
+$sql = "SELECT TrainingParticipation, TrainingWorkout, TrainingAvgHR, TrainingInjuriesOrPain FROM trainingdata";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["TrainingParticipation"]. "</td><td>" . $row["TrainingWorkout"] . "</td><td>"
+. $row["TrainingAvgHR"]. "</td></tr>" . $row["TrainingInjuriesOrPain"] . "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
 </body>
 </html>
